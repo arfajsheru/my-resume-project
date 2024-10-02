2854088597a5e607d8ef68693b8a1ce7b8990544
arfajsheru <arfatsheru74@gmail.com>
2024-10-02 13:18:07 +0530

    social medial links project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import Home from "./pages/Home";
-import Work from "./pages/work";
+
 
 const App = () => {
   return (
     <div className="overflow-x-hidden">
       <Home />
-      <Work />
-      <Contact />
     </div>
   );
 };
--- src/pages/Work.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const work = () => {
-  return (
-    <div className='w-[100vw] h-[100vh]'>work</div>
-  )
-}
-
-export default work
